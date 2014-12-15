5bb11fda6469835a52406096699e4d898897a160
Timo Ulich <timo.ulich@asideas.de>
2014-12-15 14:03:22 +0100

    @Parameters in a complex object does not work. Initialize the default value in constructor instead

--- src/main/java/de/gaffa/tools/coreos/mavenplugin/type/AvailabilityCheck.java
+++ src/main/java/de/gaffa/tools/coreos/mavenplugin/type/AvailabilityCheck.java
@@ -1,20 +1,21 @@
 package de.gaffa.tools.coreos.mavenplugin.type;
 
-import org.apache.maven.plugins.annotations.Parameter;
-
 public class AvailabilityCheck {
 
-    @Parameter(defaultValue = "false")
     private boolean enabled;
 
-    @Parameter(defaultValue = "/")
     private String contextPath;
 
-    @Parameter(defaultValue = "80")
     private int port;
 
-    @Parameter(defaultValue = "200")
     private int expectedStatusCode;
+
+    public AvailabilityCheck() {
+        this.enabled = true;
+        this.contextPath = "/";
+        this.port = 80;
+        this.expectedStatusCode = 200;
+    }
 
     public boolean isEnabled() {
         return enabled;
